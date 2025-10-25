d77cf67b28d01c18b6e80f2f0980bb5b2b70f10e
Elpeque Pro <elpeque9999@gmail.com>
2025-10-25 18:06:24 +0200

    Set login page to use dynamic rendering
    Added 'export const dynamic = "force-dynamic"' to the login page to ensure it is rendered dynamically. This may be required for authentication flows or to handle real-time data.

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -5,6 +5,8 @@ import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { FaGithub, FaGoogle } from "react-icons/fa"
 
+export const dynamic = 'force-dynamic';
+
 export default function LoginPage() {
     const supabase = createClient();
     const router = useRouter();
